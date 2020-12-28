a83fc614a3b71bf2ac275997456b76c41cb7c3b0
Ayush Dubey <72454235+AyushDubey-maker@users.noreply.github.com>
2020-12-28 10:57:40 +0530

    Updated the file to make the navbar responsive

--- index.js
+++ index.js
@@ -80,3 +80,20 @@ function setTheme(mode){
 
 	localStorage.setItem('theme', mode)
 }
+const navSlide=()=>{
+    const toggle=document.querySelector('.Toggle');
+    const nav=document.querySelector('.nav__list');
+    const navLinks=document.querySelectorAll('.nav__item');
+    toggle.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+        navLinks.forEach((link,index)=>{
+            if(link.style.animation){
+                link.style.animation='';
+            }else{
+                link.style.animation=`navLinkFade 0.5s ease forwards ${index/7+0.3}s`
+            }
+        });
+        toggle.classList.toggle('toggle');
+    });
+}
+navSlide();
